1856cd759fe77ee8a5ec46c2e6df0abe64fb0c16
juanitoOQ2 <51090514+juanitoOQ2@users.noreply.github.com>
2019-05-30 11:53:08 -0500

    from unity comentado

--- QuizzAplication/fromunity.php
+++ QuizzAplication/fromunity.php
@@ -12,12 +12,12 @@
 		echo("Field 2" . $text2);
 		echo("Field 3" . $text3);
 		//abre el archivo para escribir nuestros datos si el archivo no existe lo crea 
-		$file = fopen("data.txt","a"); // "a"  de adjuntar o agregar "w" para reemplazar o sobreescribir
-		fwrite($file, "\r\n");
-		fwrite($file, $text1);
-		fwrite($file, " respuesta1: " . $text2);
-		fwrite($file, " respuesta2: " . $text3);
-		fclose($file); 
+		//$file = fopen("data.txt","a"); // "a"  de adjuntar o agregar "w" para reemplazar o sobreescribir
+		//fwrite($file, "\r\n");
+		//fwrite($file, $text1);
+		//fwrite($file, " respuesta1: " . $text2);
+		//fwrite($file, " respuesta2: " . $text3);
+		//fclose($file); 
 	}else{
 		echo("Message delivery failed...");
 	}
